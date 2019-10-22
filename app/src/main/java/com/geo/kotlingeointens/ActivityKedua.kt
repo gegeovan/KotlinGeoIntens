a96df8865b27d53d3f055447188b6ace4a3f7a42
unknown <roghibbrr@gmail.com>
2019-10-23 06:45:25 +0700

    Initial commit

---
+++ app/src/main/java/com/geo/kotlingeointens/ActivityKedua.kt
@@ -0,0 +1,28 @@
+package com.geo.kotlingeointens
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+import kotlinx.android.synthetic.*
+
+class ActivityKedua : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.aktivity_kedua)
+
+        val intent = getIntent();
+        val nim = intent.getStringExtra("Nim")
+        val nama = intent.getStringExtra("Nama")
+        val nilai = intent.getStringExtra("Nilai")
+
+        //textview
+        val resultTv = findViewById<TextView>(R.id.resultTv)
+
+        //setText
+        resultTv.text = "Nim : "+nim+"\nNama : "+nama+"\nNilai : "+nilai
+
+
+
+    }
+}
